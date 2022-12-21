3e007d55b0f663a391157cd4693e5d7d85756cf3
karimbehery <120670524+karimbehery@users.noreply.github.com>
2022-12-21 16:11:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_numbers_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("Plaese inter n1");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter n2");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            for(int x= n1 =5; x<=n2;x++ )
+            {
+             
+                int j;
+                for (j=2;j<x;j++)
+                {
+                    if (x % j == 0)
+                    {
+                        break;
+                    
+                    }
+                    
+                }
+               if (j==x)
+                    Console.WriteLine(x);
+            }
+            Console.ReadKey();
+
+
+                
+                
+            
+        }
+    }
+}
